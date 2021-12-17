8a1260da2a6268343f7c0f98776f8a0d68e090e6
aditya <adityaajmera89@gmail.com>
2021-12-17 19:36:13 +0530

    adding initial project setup

---
+++ src/test/java/com/learning/jms/LearningJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
